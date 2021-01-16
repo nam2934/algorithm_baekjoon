9af2b91e9eb0eb1e7ad4b460747e134f691658ac
Namjin Jeong <nam2934@naver.com>
2021-01-16 06:43:53 +0000

    complete 16236.cpp

--- BFS/16236.cpp
+++ BFS/16236.cpp
@@ -10,7 +10,7 @@ vector<vector<bool> > visited;
 
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
-int length, total_time=0, shark_size=2, min_cnt, fish_x, fish_y;
+int length, total_time=0, shark_size=2, min_cnt, fish_x, fish_y, eat_count=0;
 
 void BFS(int x, int y){
     min_cnt = INT_MAX;
@@ -25,8 +25,8 @@ void BFS(int x, int y){
         int current_shark_y = Q.front().first.second;
         int cnt = Q.front().second;
         
-        if(board[current_shark_y][current_shark_x] < shark_size){
-            if(min_cnt > cnt){
+        if(cnt != 0 && board[current_shark_y][current_shark_x] < shark_size && board[current_shark_y][current_shark_x] != 0){
+            if(min_cnt >= cnt){
                 if(fish_y > current_shark_y){
                     min_cnt = cnt;
                     fish_y = current_shark_y;
@@ -43,27 +43,29 @@ void BFS(int x, int y){
         }
 
         Q.pop();
+
         for(int i=0; i<4; i++){
             int nx = current_shark_x + dx[i];
             int ny = current_shark_y + dy[i];
             if(nx >= 0 && ny >= 0 && nx < length && ny < length){
-                if(board[ny][nx] < shark_size && !visited[ny][nx]){
+                if(board[ny][nx] <= shark_size && !visited[ny][nx]){
                     Q.push(make_pair(make_pair(nx, ny),cnt+1));
                     visited[ny][nx] = true;
-                }
-                else if(board[ny][nx] == shark_size && !visited[ny][nx]){
-                    Q.push(make_pair(make_pair(nx, ny),cnt+1));
-                    visited[ny][nx] = true;     
                 }
             }
         }
     }
 }
 
+void board_set(pair<int,int> start_shark){
+    board[start_shark.second][start_shark.first] = 0;
+    board[fish_y][fish_x] = 9;
+}
+
 bool exist_fish(){
     for(int i=0; i<length; i++){
         for(int j=0; j<length; j++){
-            if(board[i][j] == 1) return true;
+            if(board[i][j] < shark_size && board[i][j] != 0) return true;
         }
     }
     return false;
@@ -75,7 +77,6 @@ int main(){
 
     cin >> length;
     board = vector<vector<int> >(length, vector<int>(length));
-    visited = vector<vector<bool> >(length, vector<bool>(length, false));
 
     for (int i = 0; i < length; i++){
         for (int j = 0; j < length; j++){
@@ -89,11 +90,22 @@ int main(){
         }
     }
     min_cnt = 0;
-    if(exist_fish()){
+    while(exist_fish()){
+        visited = vector<vector<bool> >(length, vector<bool>(length, false));
         BFS(start_shark.first, start_shark.second);
+        if(min_cnt == INT_MAX){
+            break;
+        }
+        board_set(start_shark);
+        start_shark.first = fish_x;
+        start_shark.second = fish_y;
+        eat_count++;
+        if(eat_count == shark_size){
+            shark_size++;;
+            eat_count = 0;
+        }
+        total_time += min_cnt;
+        visited.clear();
     }
-    total_time = min_cnt + total_time;
-    //board change 
-    //shark size increase
     cout << total_time;
 }
