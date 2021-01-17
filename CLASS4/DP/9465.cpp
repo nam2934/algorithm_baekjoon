8ed8897e9752a5d01ededd9c25323e38fda20225
Namjin Jeong <nam2934@naver.com>
2021-01-17 14:06:22 +0000

    make 9465.cpp

---
+++ CLASS4/DP/9465.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int n;
+
+int main(){
+    cin.tie(NULL)
+    ios_base::sync_with_stdio(false);
+    int testCase;
+    cin >> testCase;
+    while(testCase--){
+        cin >> n;
+    }
+    
+}
