49c82d582b75506c2fd87b586b1ea93a6c9eca78
Namjin Jeong <nam2934@naver.com>
2021-02-25 00:05:22 +0000

    solve 1717

---
+++ Union_Find/1717.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+
+using namespace std;
+
+vector<int> parents;
+vector<int> Rank;
+
+int Find(int a){
+    if(a == parents[a]) return a;
+    return parents[a] = Find(parents[a]);
+}
+
+void Union(int u, int v){
+    u = Find(u);
+    v = Find(v);
+    if(u == v) return;
+    if(Rank[u] > Rank[v]) swap(u,v);
+    parents[u] = v;
+}
+
+int main(){l
+    ios_base::sync_with_stdio();
+    cin.tie(NULL);
+    int n,m;
+    cin >> n >> m;
+    parents = vector<int>(n+1);
+    Rank = vector<int>(n+1, 1);
+    for(int i=0; i<=n; i++) parents[i] = i;
+    for(int i=0; i<m; i++){
+        int op, a, b;
+        cin >> op >> a >> b;
+        if(op == 0) Union(a,b);
+        else if(Find(a) == Find(b)) cout << "YES" << "\n";
+        else cout << "NO" << "\n";
+    }
+}  
