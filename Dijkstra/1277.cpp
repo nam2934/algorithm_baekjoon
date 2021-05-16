078d3fd97553d7a3a4ec04e43e98b98ae2b2569b
Namjin Jeong <nam2934@naver.com>
2021-05-16 12:46:52 +0000

    solge 1277

--- Dijkstra/1277.cpp
+++ Dijkstra/1277.cpp
@@ -5,47 +5,46 @@
 
 using namespace std;
 
-vector<pair<long, long> > board;
-vector<vector<bool> > wire;
-vector<pair<int, pair<int, int> > > adj[1001];
+vector<pair<int, int> > board;
+vector<pair<int, double> > adj[1001];
 
 int N,W;
 double M;
 
-const int INF = 987654321;
+double get_dist(int x1, int x2, int y1, int y2){
+    return sqrt(pow(x1-x2 , 2)+pow(y1-y2, 2));
+}
 
 void make_graph(){
-    for(int i=1; i<=N; i++){
-        long x = board[i].first;
-        long y = board[i].second;
-        for(int j=1; j<=N; j++){
-            if(i == j) continue;
-            long nx = board[j].first;
-            long ny = board[j].second;
-            long pow_result = (nx-x)*(nx-x)+(ny-y)*(ny-y);
-            double w = sqrt(pow_result);
-            if(M >= w){
-                if(wire[i][j]) adj[i].push_back(make_pair(j, make_pair(0, 0)));
-                else adj[i].push_back(make_pair(j, make_pair(ny-y, nx-x)));
+    for(int i=1; i<N; i++){
+        for(int j=i; j<=N; j++){
+            int x = board[i].first;
+            int y = board[i].second;
+            int nx = board[j].first;
+            int ny = board[j].second;
+            double w = get_dist(x,nx,y,ny);
+            if(w <= M){
+                adj[i].push_back(make_pair(j, w));
+                adj[j].push_back(make_pair(i, w));
             }
         }
     }
 }
 
 vector<double> Dijkstra(int src){
-    vector<double> dist(N+1, INF);
-    dist[src] = 0.0;
+    vector<double> dist(N+1, 1.79769e+308);
+    dist[src] = 0;
     priority_queue<pair<double, int> > pq;
-    pq.push(make_pair(0.0, src));
+    pq.push(make_pair(0, src));
     while(!pq.empty()){
         int here = pq.top().second;
         double cur_dist = -pq.top().first;
         pq.pop();
         if(dist[here] < cur_dist) continue;
+        dist[here] = cur_dist;
         for(int i=0; i<adj[here].size(); i++){
             int there = adj[here][i].first;
-            double next_dist = sqrt(adj[here][i].second.first*adj[here][i].second.first + adj[here][i].second.second*adj[here][i].second.second);
-            next_dist = cur_dist + next_dist;
+            double next_dist = cur_dist + adj[here][i].second;
             if(dist[there] > next_dist){
                 dist[there] = next_dist;
                 pq.push(make_pair(-next_dist, there));
@@ -61,21 +60,20 @@ int main(){
 
     cin >> N >> W;
     cin >> M;
-    wire = vector<vector<bool> >(N+1, vector<bool>(N+1, false));
-    board = vector<pair<long, long> >(N+1);
+    board = vector<pair<int, int> >(N+1);
     for(int i=1; i<=N; i++){
-        long x, y;
+        int x, y;
         cin >> x >> y;
         board[i] = make_pair(x,y);
     }
     for(int i=0; i<W; i++){
         int v1, v2;
         cin >> v1 >> v2;
-        wire[v1][v2] = true;
-        wire[v2][v1] = true;
+        adj[v1].push_back(make_pair(v2,0));
+        adj[v2].push_back(make_pair(v1,0));
     }
     make_graph();
     vector<double> result = Dijkstra(1);
-    if(result[N] == INF) cout << -1 << endl;
-    else cout << floor(result[N]*1000) << endl;
+    cout << (long long)(result[N]*1000);
 }
+
--- Dijkstra/tempCodeRunnerFile.cpp
+++ Dijkstra/tempCodeRunnerFile.cpp
@@ -1,28 +1,53 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<cmath>
+#include<climits>
+#define DBL_MAX 1.7976931348623158e+308
+
 using namespace std;
 
-vector<pair<int, int> > adj[10001];
+vector<pair<int, int> > board;
+vector<vector<bool> > wire;
+vector<pair<int, double> > adj[1001];
 
-const int INF = 1987654321;
-int computer;
-vector<int> Dijkstra(int start){
-    vector<int> dist(computer+1);
-    priority_queue<pair<int, int> > pq;
-    pq.push(make_pair(0, start));
-    dist[start] = 0;
+int N,W;
+double M;
+
+void make_graph(){
+    for(int i=1; i<N; i++){
+        int x = board[i].first;
+        int y = board[i].second;
+        for(int j=i; j<=N; j++){
+            if(i == j) continue;
+            int nx = board[j].first;
+            int ny = board[j].second;
+            double w = sqrt((nx-x)*(nx-x)+(ny-y)*(ny-y));
+            if(w <= M){
+                if(wire[i][j]) adj[i].push_back(make_pair(j, 0));
+                else adj[i].push_back(make_pair(j, w));
+            }
+        }
+    }
+}
+
+vector<double> Dijkstra(int src){
+    vector<double> dist(N+1, DBL_MAX);
+    dist[src] = 0;
+    priority_queue<pair<double, int> > pq;
+    pq.push(make_pair(0, src));
     while(!pq.empty()){
         int here = pq.top().second;
-        int cur_dist = -pq.top().first;
+        double cur_dist = -pq.top().first;
         pq.pop();
-        if(cur_dist > dist[here]) continue;
+        if(dist[here] < cur_dist) continue;
+        dist[here] = cur_dist;
         for(int i=0; i<adj[here].size(); i++){
             int there = adj[here][i].first;
-            int next_dist = adj[here][i].second + cur_dist;
-            if(next_dist < dist[there]){
-                pq.push(make_pair(-next_dist, there));
+            double next_dist = cur_dist + adj[here][i].second;
+            if(dist[there] > next_dist){
                 dist[there] = next_dist;
+                pq.push(make_pair(-next_dist, there));
             }
         }
     }
@@ -32,27 +57,24 @@ vector<int> Dijkstra(int start){
 int main(){
     ios_base::sync_with_stdio();
     cin.tie(NULL);
-    int testcase;
-    cin >> testcase;
-    while(testcase--){
-        for(int i=0; i<10001; i++) adj[i].clear();
-        int dependency, start_v;
-        cin >> computer >> dependency >> start_v;
-        for(int i=0; i<dependency; i++){
-            int v1, v2, w;
-            cin >> v1 >> v2 >> w;
-            adj[v2].push_back(make_pair(v1, w));
-        }
-        vector<int> dist = Dijkstra(start_v);
-        int result_time = 0;
-        int result_count = 0;
-        for(int i=0; i<dist.size(); i++){
-            if(dist[i] == INF) continue;
-            result_count++;
-            if(dist[i] > result_time){
-                result_time = dist[i];
-            }
-        }
-        cout << result_count << " " << result_time << "\n";
+
+    cin >> N >> W;
+    cin >> M;
+    wire = vector<vector<bool> >(N+1, vector<bool>(N+1, false));
+    board = vector<pair<int, int> >(N+1);
+    for(int i=1; i<=N; i++){
+        int x, y;
+        cin >> x >> y;
+        board[i] = make_pair(x,y);
     }
+    for(int i=0; i<W; i++){
+        int v1, v2;
+        cin >> v1 >> v2;
+        wire[v1][v2] = true;
+        wire[v2][v1] = true;
+    }
+    make_graph();
+    vector<double> result = Dijkstra(1);
+    cout << (long long)result[N]*1000 << endl;
 }
+
