83d60193db8c1c9a34006bdbc8d8edfb89e360fc
Namjin Jeong <nam2934@naver.com>
2021-05-22 09:35:20 +0000

    solve 4781

---
+++ CLASS5/DP/Knapsack_Problem/4781.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<cstring>
+using namespace std;
+
+int n, money, result;
+float m, each_money;;
+int C[5001], P[5001], DP[10001];
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    while(true){
+        result = 0;
+        cin >> n >> m;
+        money = (int)(m*100+0.5);
+        memset(C,0,sizeof(C));
+        memset(P,0,sizeof(P));
+        memset(DP,0,sizeof(DP));
+        if(n == 0 && m == 0.00) break;
+        for(int i=0; i<n; i++){
+            cin >> C[i] >> each_money;
+            P[i] = (int)(each_money*100+0.5);
+        }
+        for(int i=0; i<n; i++){
+            for(int j=P[i]; j<=money; j++){
+                DP[j] = max(DP[j-P[i]]+C[i],DP[j]);
+                result = max(result, DP[j]);
+            }
+        }
+        cout << result << "\n";
+    }
+}
