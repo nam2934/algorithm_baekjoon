ca28fac0e11a7b159645944d6582ac3692e8487d
Namjin Jeong <nam2934@naver.com>
2021-05-23 01:21:25 +0000

    solve 1450

--- CLASS5/DP/Knapsack_Problem/1450.cpp
+++ CLASS5/DP/Knapsack_Problem/1450.cpp
@@ -4,27 +4,28 @@
 
 using namespace std;
 
-int N,C,result = 0;
-int W[31];
-vector<int> g1,g2;
+long long result = 0;
+int N,C;
+long long W[31];
+vector<long long> g1,g2;
 
-void BF(int s, int e, vector<int>& g, int sum){
+void BF(int s, int e, vector<long long>& g, long long sum){
     if(s > e){
         g.push_back(sum);
         return;
     }
+    BF(s+1,e,g,sum);
     BF(s+1,e,g,sum+W[s]);
-    BF(s,e,g,sum+W[s]);
 }
 
 int main(){
     cin >> N >> C;
     for(int i=0; i<N; i++) cin >> W[i];
     BF(0,N/2,g1,0);
-    BF(N/2+1,N,g2,0);
+    BF(N/2+1,N-1,g2,0);
     sort(g2.begin(), g2.end());
     for(int i=0; i<g1.size(); i++){
-        if(C-g1[i] > 0) result += upper_bound(g2.begin(), g2.end(), C-g1[i]) - g2.begin();
+        result += (upper_bound(g2.begin(), g2.end(), C-g1[i]) - g2.begin());
     }
     cout << result;
 }
